7fbe1b2b25366b6a2ea9b897f86ebc9913496327
Elizaveta Biryukova <birliza@mail.ru>
2022-08-02 15:49:11 +0200

    change buttons in slider

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -1,24 +1,20 @@
-const searchImages = document.querySelector('.search-restaurant__images');
-const btnControl = document.querySelector('.search-restaurant__btn-control');
-const searchPicture = document.querySelector('.search-restaurant__picture');
-const searchPictureActive = document.querySelector('.search-restaurant__picture--active');
-const btnNext = document.querySelector('.search-restaurant__btn-control--next');
-const btnBack = document.querySelector('.search-restaurant__btn-control--back');
+// const sliderImages = document.querySelector('.slider__images');
+// const btnControl = document.querySelector('.slider__btn-control');
+const sliderPictures = document.querySelectorAll('.slider__picture');
+const btnNext = document.querySelector('.slider__btn-control--next');
+const btnBack = document.querySelector('.slider__btn-control--back');
 
 let index = 0;
 
-// btnControl.addEventListener('click', )
-
 const activeSlide = n => {
-    console.log(n);
-    for (slide of searchImages) {
-        slide.classList.remove('search-restaurant__picture--active');
-    }
-    searchImages[n].classList.add('search-restaurant__picture--active');
+    sliderPictures.forEach(element => {
+        element.classList.remove('slider__picture--active');
+    });
+    sliderPictures[n].classList.add('slider__picture--active');
 };
 
 const nextSlide = () => {
-    if (index == searchImages.length - 1) {
+    if (index == sliderPictures.length - 1) {
         index = 0;
         activeSlide(index);
     } else {
